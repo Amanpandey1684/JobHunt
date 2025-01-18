0bdfebef818d3659c98b4874a0701f667f577ca0
devps814 <dev.singh_cs22@gla.ac.in>
2025-01-19 02:48:26 +0530

    setup for mongodb
    create a database and connect with server . create user schema for types of data in login and register. create register for student and recruiter.

---
+++ backend/models/company.model.js
@@ -0,0 +1,24 @@
+import { type } from "@testing-library/user-event/dist/type";
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    website: {
+        type: String,
+    },
+    logo: {
+        type: String, //URL for company logo
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+},{timestamps:true});
+export const Company = mongoose.model("Company", companySchema);
