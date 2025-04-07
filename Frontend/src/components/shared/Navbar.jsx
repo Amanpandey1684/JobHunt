c496c54279ef746bab5e8651eca0d8d6cbf902b9
Aman Pandey <aman.pandey_cs22@gla.ac.in>
2025-04-07 15:30:03 +0530

    fix: update branding from "Job Hunt" to "Job Khojo" in Footer and Navbar components

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className='bg-gray-100 shadow-md bg-opacity-50 z-50'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Hunt</span></h1>
+                    <h1 className='text-2xl font-bold'>Job<span className='text-[#F83002]'>Khojo</span></h1>
                 </div>
                 <div className='flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
